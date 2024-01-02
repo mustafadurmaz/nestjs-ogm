147510b9b2900120794af3649657c62a6a6cfdaa
mustafadurmaz <mdmz_001@hotmail.com>
2024-01-02 23:29:34 +0300

    neo4j config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { Neo4jModule } from 'sgnm-neo4j/dist';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    Neo4jModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        host: configService.get('NEO4J_HOST'),
+        password: configService.get('NEO4J_PASSWORD'),
+        port: configService.get('NEO4J_PORT'),
+        scheme: configService.get('NEO4J_SCHEME'),
+        username: configService.get('NEO4J_USERNAME'),
+        database: configService.get('NEO4J_DATABASE'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
