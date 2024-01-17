f8662166e437444ca9a3162349de93c62880d3ad
mustafadurmaz <mdmz_001@hotmail.com>
2024-01-17 22:52:41 +0300

    user type definition edit

---
+++ src/ogm/types/type-definition.ogm.GENERIC.d.ts
@@ -0,0 +1,312 @@
+import type { SelectionSetNode, DocumentNode } from "graphql";
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+  T extends { [key: string]: unknown },
+  K extends keyof T
+> = { [_ in K]?: never };
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
+    };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string };
+  /** The `String` scalar type represents textual data, represented as UTF-8 character sequences. The String type is most often used by GraphQL to represent free-form human-readable text. */
+  String: { input: string; output: string };
+  /** The `Boolean` scalar type represents `true` or `false`. */
+  Boolean: { input: boolean; output: boolean };
+  /** The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1. */
+  Int: { input: number; output: number };
+  /** The `Float` scalar type represents signed double-precision fractional values as specified by [IEEE 754](https://en.wikipedia.org/wiki/IEEE_floating_point). */
+  Float: { input: number; output: number };
+};
+
+export type Query = {
+  __typename?: "Query";
+  users: Array<User>;
+  usersConnection: UsersConnection;
+  usersAggregate: UserAggregateSelection;
+};
+
+export type QueryUsersArgs = {
+  where?: InputMaybe<UserWhere>;
+  options?: InputMaybe<UserOptions>;
+};
+
+export type QueryUsersConnectionArgs = {
+  first?: InputMaybe<Scalars["Int"]["input"]>;
+  after?: InputMaybe<Scalars["String"]["input"]>;
+  where?: InputMaybe<UserWhere>;
+  sort?: InputMaybe<Array<InputMaybe<UserSort>>>;
+};
+
+export type QueryUsersAggregateArgs = {
+  where?: InputMaybe<UserWhere>;
+};
+
+export type Mutation = {
+  __typename?: "Mutation";
+  createUsers: CreateUsersMutationResponse;
+  deleteUsers: DeleteInfo;
+  updateUsers: UpdateUsersMutationResponse;
+};
+
+export type MutationCreateUsersArgs = {
+  input: Array<UserCreateInput>;
+};
+
+export type MutationDeleteUsersArgs = {
+  where?: InputMaybe<UserWhere>;
+};
+
+export type MutationUpdateUsersArgs = {
+  where?: InputMaybe<UserWhere>;
+  update?: InputMaybe<UserUpdateInput>;
+};
+
+/** An enum for sorting in either ascending or descending order. */
+export enum SortDirection {
+  /** Sort by field values in ascending order. */
+  Asc = "ASC",
+  /** Sort by field values in descending order. */
+  Desc = "DESC",
+}
+
+/** Information about the number of nodes and relationships created during a create mutation */
+export type CreateInfo = {
+  __typename?: "CreateInfo";
+  /** @deprecated This field has been deprecated because bookmarks are now handled by the driver. */
+  bookmark?: Maybe<Scalars["String"]["output"]>;
+  nodesCreated: Scalars["Int"]["output"];
+  relationshipsCreated: Scalars["Int"]["output"];
+};
+
+export type CreateUsersMutationResponse = {
+  __typename?: "CreateUsersMutationResponse";
+  info: CreateInfo;
+  users: Array<User>;
+};
+
+/** Information about the number of nodes and relationships deleted during a delete mutation */
+export type DeleteInfo = {
+  __typename?: "DeleteInfo";
+  /** @deprecated This field has been deprecated because bookmarks are now handled by the driver. */
+  bookmark?: Maybe<Scalars["String"]["output"]>;
+  nodesDeleted: Scalars["Int"]["output"];
+  relationshipsDeleted: Scalars["Int"]["output"];
+};
+
+export type IntAggregateSelectionNullable = {
+  __typename?: "IntAggregateSelectionNullable";
+  max?: Maybe<Scalars["Int"]["output"]>;
+  min?: Maybe<Scalars["Int"]["output"]>;
+  average?: Maybe<Scalars["Float"]["output"]>;
+  sum?: Maybe<Scalars["Int"]["output"]>;
+};
+
+/** Pagination information (Relay) */
+export type PageInfo = {
+  __typename?: "PageInfo";
+  hasNextPage: Scalars["Boolean"]["output"];
+  hasPreviousPage: Scalars["Boolean"]["output"];
+  startCursor?: Maybe<Scalars["String"]["output"]>;
+  endCursor?: Maybe<Scalars["String"]["output"]>;
+};
+
+export type StringAggregateSelectionNullable = {
+  __typename?: "StringAggregateSelectionNullable";
+  shortest?: Maybe<Scalars["String"]["output"]>;
+  longest?: Maybe<Scalars["String"]["output"]>;
+};
+
+/** Information about the number of nodes and relationships created and deleted during an update mutation */
+export type UpdateInfo = {
+  __typename?: "UpdateInfo";
+  /** @deprecated This field has been deprecated because bookmarks are now handled by the driver. */
+  bookmark?: Maybe<Scalars["String"]["output"]>;
+  nodesCreated: Scalars["Int"]["output"];
+  nodesDeleted: Scalars["Int"]["output"];
+  relationshipsCreated: Scalars["Int"]["output"];
+  relationshipsDeleted: Scalars["Int"]["output"];
+};
+
+export type UpdateUsersMutationResponse = {
+  __typename?: "UpdateUsersMutationResponse";
+  info: UpdateInfo;
+  users: Array<User>;
+};
+
+export type User = {
+  __typename?: "User";
+  id?: Maybe<Scalars["Int"]["output"]>;
+  name?: Maybe<Scalars["String"]["output"]>;
+  email?: Maybe<Scalars["String"]["output"]>;
+};
+
+export type UserAggregateSelection = {
+  __typename?: "UserAggregateSelection";
+  count: Scalars["Int"]["output"];
+  id: IntAggregateSelectionNullable;
+  name: StringAggregateSelectionNullable;
+  email: StringAggregateSelectionNullable;
+};
+
+export type UserEdge = {
+  __typename?: "UserEdge";
+  cursor: Scalars["String"]["output"];
+  node: User;
+};
+
+export type UsersConnection = {
+  __typename?: "UsersConnection";
+  totalCount: Scalars["Int"]["output"];
+  pageInfo: PageInfo;
+  edges: Array<UserEdge>;
+};
+
+export type UserCreateInput = {
+  id?: InputMaybe<Scalars["Int"]["input"]>;
+  name?: InputMaybe<Scalars["String"]["input"]>;
+  email?: InputMaybe<Scalars["String"]["input"]>;
+};
+
+export type UserOptions = {
+  limit?: InputMaybe<Scalars["Int"]["input"]>;
+  offset?: InputMaybe<Scalars["Int"]["input"]>;
+  /** Specify one or more UserSort objects to sort Users by. The sorts will be applied in the order in which they are arranged in the array. */
+  sort?: InputMaybe<Array<UserSort>>;
+};
+
+/** Fields to sort Users by. The order in which sorts are applied is not guaranteed when specifying many fields in one UserSort object. */
+export type UserSort = {
+  id?: InputMaybe<SortDirection>;
+  name?: InputMaybe<SortDirection>;
+  email?: InputMaybe<SortDirection>;
+};
+
+export type UserUpdateInput = {
+  id?: InputMaybe<Scalars["Int"]["input"]>;
+  id_INCREMENT?: InputMaybe<Scalars["Int"]["input"]>;
+  id_DECREMENT?: InputMaybe<Scalars["Int"]["input"]>;
+  name?: InputMaybe<Scalars["String"]["input"]>;
+  email?: InputMaybe<Scalars["String"]["input"]>;
+};
+
+export type UserWhere = {
+  id?: InputMaybe<Scalars["Int"]["input"]>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  id_NOT?: InputMaybe<Scalars["Int"]["input"]>;
+  id_IN?: InputMaybe<Array<InputMaybe<Scalars["Int"]["input"]>>>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  id_NOT_IN?: InputMaybe<Array<InputMaybe<Scalars["Int"]["input"]>>>;
+  id_LT?: InputMaybe<Scalars["Int"]["input"]>;
+  id_LTE?: InputMaybe<Scalars["Int"]["input"]>;
+  id_GT?: InputMaybe<Scalars["Int"]["input"]>;
+  id_GTE?: InputMaybe<Scalars["Int"]["input"]>;
+  name?: InputMaybe<Scalars["String"]["input"]>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  name_NOT?: InputMaybe<Scalars["String"]["input"]>;
+  name_IN?: InputMaybe<Array<InputMaybe<Scalars["String"]["input"]>>>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  name_NOT_IN?: InputMaybe<Array<InputMaybe<Scalars["String"]["input"]>>>;
+  name_CONTAINS?: InputMaybe<Scalars["String"]["input"]>;
+  name_STARTS_WITH?: InputMaybe<Scalars["String"]["input"]>;
+  name_ENDS_WITH?: InputMaybe<Scalars["String"]["input"]>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  name_NOT_CONTAINS?: InputMaybe<Scalars["String"]["input"]>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  name_NOT_STARTS_WITH?: InputMaybe<Scalars["String"]["input"]>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  name_NOT_ENDS_WITH?: InputMaybe<Scalars["String"]["input"]>;
+  email?: InputMaybe<Scalars["String"]["input"]>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  email_NOT?: InputMaybe<Scalars["String"]["input"]>;
+  email_IN?: InputMaybe<Array<InputMaybe<Scalars["String"]["input"]>>>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  email_NOT_IN?: InputMaybe<Array<InputMaybe<Scalars["String"]["input"]>>>;
+  email_CONTAINS?: InputMaybe<Scalars["String"]["input"]>;
+  email_STARTS_WITH?: InputMaybe<Scalars["String"]["input"]>;
+  email_ENDS_WITH?: InputMaybe<Scalars["String"]["input"]>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  email_NOT_CONTAINS?: InputMaybe<Scalars["String"]["input"]>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  email_NOT_STARTS_WITH?: InputMaybe<Scalars["String"]["input"]>;
+  /** @deprecated Negation filters will be deprecated, use the NOT operator to achieve the same behavior */
+  email_NOT_ENDS_WITH?: InputMaybe<Scalars["String"]["input"]>;
+  OR?: InputMaybe<Array<UserWhere>>;
+  AND?: InputMaybe<Array<UserWhere>>;
+  NOT?: InputMaybe<UserWhere>;
+};
+
+export interface IntAggregateInputNullable {
+  max?: boolean;
+  min?: boolean;
+  average?: boolean;
+  sum?: boolean;
+}
+export interface StringAggregateInputNullable {
+  shortest?: boolean;
+  longest?: boolean;
+}
+export interface UserAggregateSelectionInput {
+  count?: boolean;
+  id?: IntAggregateInputNullable;
+  name?: StringAggregateInputNullable;
+  email?: StringAggregateInputNullable;
+}
+
+export declare class UserModel {
+  public find(args?: {
+    where?: UserWhere;
+
+    options?: UserOptions;
+    selectionSet?: string | DocumentNode | SelectionSetNode;
+    args?: any;
+    context?: any;
+    rootValue?: any;
+  }): Promise<User[]>;
+  public create(args: {
+    input: UserCreateInput[];
+    selectionSet?: string | DocumentNode | SelectionSetNode;
+    args?: any;
+    context?: any;
+    rootValue?: any;
+  }): Promise<CreateUsersMutationResponse>;
+  public update(args: {
+    where?: UserWhere;
+    update?: UserUpdateInput;
+
+    selectionSet?: string | DocumentNode | SelectionSetNode;
+    args?: any;
+    context?: any;
+    rootValue?: any;
+  }): Promise<UpdateUsersMutationResponse>;
+  public delete(args: {
+    where?: UserWhere;
+
+    context?: any;
+    rootValue?: any;
+  }): Promise<{ nodesDeleted: number; relationshipsDeleted: number }>;
+  public aggregate(args: {
+    where?: UserWhere;
+
+    aggregate: UserAggregateSelectionInput;
+    context?: any;
+    rootValue?: any;
+  }): Promise<UserAggregateSelection>;
+}
+
+export interface ModelMap {
+  User: UserModel;
+}
--- src/user/types/user.type.definition.ts
+++ src/user/types/user.type.definition.ts
@@ -6,6 +6,7 @@ type ${EnumList.USER} @mutation(operations: []) {
 	id: Int @unique
     name:String
     email:String
+}
  `;
 
 
