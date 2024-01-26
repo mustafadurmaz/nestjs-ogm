6734e9b1fb27811287271ef5b2a252661868e2b4
mustafadurmaz <mdmz_001@hotmail.com>
2024-01-26 22:31:41 +0300

    type definition edit

--- src/user/types/user.type.definition.ts
+++ src/user/types/user.type.definition.ts
@@ -1,20 +1,24 @@
-import { EnumList } from 'src/common/enum/enumList'
+import { EnumList } from 'src/common/enum/enumList';
 export const UserModuleSchema = `#graphql
 
+interface ParentOfProperties @relationshipProperties { 
+    isDeleted: Boolean!
+    id:Int
+  }
+
 type ${EnumList.USER} @mutation(operations: []) { 
-	  id: Int @unique
+	id: Int @unique
     name:String
     email:String
-    subUsers: [${EnumList.SUBUSER}]
+    subSla: [SubUser!]!  @relationship(type: "PARENT_OF", direction: OUT, properties: "ParentOfProperties") 
 }
 
 type ${EnumList.SUBUSER} @mutation(operations: []) { 
-	  id: Int @unique
+	id: Int @unique
     name:String
     email:String
-    user: ${EnumList.USER}
+    parentOfUser: [User!]! @relationship(type: "PARENT_OF", direction: IN, properties: "ParentOfProperties")
 }
  `;
 
-
-export default UserModuleSchema 
+export default UserModuleSchema;
